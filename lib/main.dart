9fc02913fb319db209c90ad91be8bdea3caeeb43
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2021-11-02 08:10:36 -0500

    feat(scan_list_provider): notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_scanner/providers/scan_list_provider.dart';
 import 'package:qr_scanner/providers/ui_provider.dart';
 import 'package:qr_scanner/screens/home_screen.dart';
 import 'package:qr_scanner/screens/map_screen.dart';
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => UiProvider())
+        ChangeNotifierProvider(create: (_) => UiProvider()),
+        ChangeNotifierProvider(create: (_) => ScanListProvider())
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
---
+++ lib/providers/scan_list_provider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:qr_scanner/providers/db_provider.dart';
+
+class ScanListProvider extends ChangeNotifier {
+
+  List<ScanModel> scans = [];
+  String selectedType = 'http';
+
+  void newScan( String value ) async {
+    final newScan = ScanModel(value: value);
+    final id = await DBProvider.db.newScan(newScan);
+    newScan.id = id;
+
+    if (selectedType == newScan.type) {
+      scans.add(newScan);
+      notifyListeners();
+    }
+  }
+  
+  void loadScans() async {
+    final allScans = await DBProvider.db.getAllScans();
+    scans = [...allScans];
+    notifyListeners();
+  }
+
+  void loadScansByType(String type) async {
+    final allScans = await DBProvider.db.getScansByType(type);
+    scans = [...allScans];
+    selectedType = type;
+    notifyListeners();
+  }
+
+  void deleteScanById(int id) async {
+    await DBProvider.db.deleteScan(id);
+    loadScansByType(selectedType);
+  }
+
+  void deleteAllScans() async {
+    await DBProvider.db.deleteAllScans();
+    scans = [];
+    notifyListeners();
+  }
+}
