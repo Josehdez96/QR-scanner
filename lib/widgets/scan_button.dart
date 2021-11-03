c779f066d8b4d20850d36ae9957adeac47fa50f0
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2021-11-03 08:00:03 -0500

    feat(scan_button): add new scan to the history

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_scanner/providers/scan_list_provider.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final scanListProvider = Provider.of<ScanListProvider>(context, listen: false);
     return FloatingActionButton(
       child: const Icon(Icons.filter_center_focus),
-      onPressed: () {}
+      onPressed: () async {
+
+        scanListProvider.newScan('https://loquesea123.com');
+        scanListProvider.newScan('geo:123');
+      }
     );
   }
 }
