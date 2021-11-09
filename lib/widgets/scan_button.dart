459a6e4fd22af2f0cf99c24011aa3655e72d7e72
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2021-11-09 08:24:50 -0500

    looking for a QR Code image reader

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:provider/provider.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:qr_scanner/providers/scan_list_provider.dart';
 import 'package:qr_scanner/utils/utils.dart';
 import 'package:qr_scanner/widgets/circular_button.dart';
@@ -71,10 +72,20 @@ class _ScanButtonState extends State<ScanButton> with SingleTickerProviderStateM
             offset: Offset.fromDirection(getRadiansFromDegree(315), degOneTranslationAnimation.value * 80),
             child: CircularButton(
               width: 54,
-              height: 54, 
+              height: 54,
               color: Colors.deepPurple,
               icon: const Icon(Icons.insert_photo), 
-              onPressed: () => print('Buenas 2')
+              onPressed: () async {
+                final ImagePicker _picker = ImagePicker();
+                final XFile? qrImageFile = await _picker.pickImage(source: ImageSource.gallery);
+                print(qrImageFile?.path);
+
+                if (qrImageFile == null) {
+                  // picking image was cancel
+                  return;
+                }
+
+              }
             )
           ),
     
