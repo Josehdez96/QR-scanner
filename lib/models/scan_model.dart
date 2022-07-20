85b678b8f2b9a48077cef5c4e573a48b89c4043e
Jose Daniel Hernandez Quiceno <jd.hernandez@globant.com>
2022-07-20 15:34:52 -0500

    fix: lat and lng and replacing deprecated property

--- lib/models/scan_model.dart
+++ lib/models/scan_model.dart
@@ -1,4 +1,3 @@
-
 // To parse this JSON data, do
 //
 //     final scanModel = scanModelFromMap(jsonString);
@@ -8,42 +7,42 @@ import 'dart:convert';
 import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 class ScanModel {
-    ScanModel({
-        this.id,
-        this.type,
-        required this.value,
-    }) {
-      if (value.contains('http')) {
-        type = 'http';
-      } else {
-        type = 'geo';
-      }
+  ScanModel({
+    this.id,
+    this.type,
+    required this.value,
+  }) {
+    if (value.contains('http')) {
+      type = 'http';
+    } else {
+      type = 'geo';
     }
+  }
 
-    int? id;
-    String? type;
-    String value;
+  int? id;
+  String? type;
+  String value;
 
-    LatLng getLatLng() {
-      final latLng = value.substring(4).split(',');
-      final parsedLat = double.parse(latLng[0]);
-      final parsedLng = double.parse(latLng[1]);
-      return LatLng(parsedLat, parsedLng);
-    }
+  LatLng getLatLng() {
+    final latLng = value.substring(4).split(',');
+    final parsedLat = double.parse(latLng[0]);
+    final parsedLng = double.parse(latLng[2]);
+    return LatLng(parsedLat, parsedLng);
+  }
 
-    factory ScanModel.fromJson(String str) => ScanModel.fromMap(json.decode(str));
+  factory ScanModel.fromJson(String str) => ScanModel.fromMap(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+  String toJson() => json.encode(toMap());
 
-    factory ScanModel.fromMap(Map<String, dynamic> json) => ScanModel(
+  factory ScanModel.fromMap(Map<String, dynamic> json) => ScanModel(
         id: json["id"],
         type: json["type"],
         value: json["value"],
-    );
+      );
 
-    Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() => {
         "id": id,
         "type": type,
         "value": value,
-    };
+      };
 }
--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -8,24 +8,28 @@ import 'package:qr_scanner/utils/utils.dart';
 import 'package:qr_scanner/widgets/circular_button.dart';
 
 class ScanButton extends StatefulWidget {
-  const ScanButton({ Key? key }) : super(key: key);
+  const ScanButton({Key? key}) : super(key: key);
 
   @override
   State<ScanButton> createState() => _ScanButtonState();
 }
 
-class _ScanButtonState extends State<ScanButton> with SingleTickerProviderStateMixin {
+class _ScanButtonState extends State<ScanButton>
+    with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Animation degOneTranslationAnimation;
-  
 
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 150));
-    degOneTranslationAnimation = Tween(begin: 0.0, end: 1.0).animate(animationController);
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+    );
+    degOneTranslationAnimation =
+        Tween(begin: 0.0, end: 1.0).animate(animationController);
     animationController.addListener(() {
-      setState(() { });
+      setState(() {});
     });
   }
 
@@ -37,76 +41,79 @@ class _ScanButtonState extends State<ScanButton> with SingleTickerProviderStateM
   Future<String?> readBarCode(XFile qrImageFile) async {
     final inputImage = InputImage.fromFilePath(qrImageFile.path);
     final barcodeScanner = GoogleMlKit.vision.barcodeScanner();
-    final List<Barcode> barcodes = await barcodeScanner.processImage(inputImage);
-    return barcodes[0].value.displayValue;
+    final List<Barcode> barcodes =
+        await barcodeScanner.processImage(inputImage);
+    return barcodes[0].displayValue;
   }
 
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context, listen: false);
+    final scanListProvider =
+        Provider.of<ScanListProvider>(context, listen: false);
     return Padding(
       padding: const EdgeInsets.only(bottom: 40),
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          IgnorePointer(
-            child: Container(
-              width: 150,
-              height: 150,
-              color: Colors.transparent,
+      child: Stack(alignment: Alignment.bottomCenter, children: [
+        IgnorePointer(
+          child: Container(
+            width: 150,
+            height: 150,
+            color: Colors.transparent,
+          ),
+        ),
+        Transform.translate(
+            offset: Offset.fromDirection(
+              getRadiansFromDegree(225),
+              degOneTranslationAnimation.value * 80,
             ),
-          ),
-    
-          Transform.translate(
-            offset: Offset.fromDirection(getRadiansFromDegree(225), degOneTranslationAnimation.value * 80),
             child: CircularButton(
-              width: 54,
-              height: 54, 
-              color: Colors.deepPurple,
-              icon: const Icon(Icons.camera_alt_rounded), 
-              onPressed: () async {
-                String scannedData = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancel', false, ScanMode.QR);
-        
-                if (scannedData == '-1') {
-                  // scanned was cancel
-                  return;
-                }
-                final newScan = await scanListProvider.newScan(scannedData);
-                launchURL(context, newScan);
-              }
-            )
+                width: 54,
+                height: 54,
+                color: Colors.deepPurple,
+                icon: const Icon(Icons.camera_alt_rounded),
+                onPressed: () async {
+                  String scannedData = await FlutterBarcodeScanner.scanBarcode(
+                      '#3D8BEF', 'Cancel', false, ScanMode.QR);
+
+                  if (scannedData == '-1') {
+                    // scanned was cancel
+                    return;
+                  }
+                  final newScan = await scanListProvider.newScan(scannedData);
+                  launchURL(context, newScan);
+                })),
+        Transform.translate(
+          offset: Offset.fromDirection(
+            getRadiansFromDegree(315),
+            degOneTranslationAnimation.value * 80,
           ),
-    
-          Transform.translate(
-            offset: Offset.fromDirection(getRadiansFromDegree(315), degOneTranslationAnimation.value * 80),
-            child: CircularButton(
-              width: 54,
-              height: 54,
-              color: Colors.deepPurple,
-              icon: const Icon(Icons.insert_photo), 
-              onPressed: () async {
-                final ImagePicker _picker = ImagePicker();
-                final XFile? qrImageFile = await _picker.pickImage(source: ImageSource.gallery);
+          child: CircularButton(
+            width: 54,
+            height: 54,
+            color: Colors.deepPurple,
+            icon: const Icon(Icons.insert_photo),
+            onPressed: () async {
+              final ImagePicker _picker = ImagePicker();
+              final XFile? qrImageFile =
+                  await _picker.pickImage(source: ImageSource.gallery);
 
-                if (qrImageFile == null) {
-                  // picking image was cancel
-                  return;
-                }
-
-                String? scannedData = await readBarCode(qrImageFile);
-
-                if (scannedData == null) {
-                  // error reading QR
-                  return;
-                }
-
-                final newScan = await scanListProvider.newScan(scannedData);
-                launchURL(context, newScan);
+              if (qrImageFile == null) {
+                // picking image was cancel
+                return;
               }
-            )
+
+              String? scannedData = await readBarCode(qrImageFile);
+
+              if (scannedData == null) {
+                // error reading QR
+                return;
+              }
+
+              final newScan = await scanListProvider.newScan(scannedData);
+              launchURL(context, newScan);
+            },
           ),
-    
-          FloatingActionButton(
+        ),
+        FloatingActionButton(
             child: const Icon(Icons.filter_center_focus),
             onPressed: () {
               if (animationController.isCompleted) {
@@ -114,10 +121,8 @@ class _ScanButtonState extends State<ScanButton> with SingleTickerProviderStateM
               } else {
                 animationController.forward();
               }
-            }
-          ),
-        ] 
-      ),
+            }),
+      ]),
     );
   }
 }
